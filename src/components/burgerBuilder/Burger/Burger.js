d67d22f957476518a240be1605f0ba60704b2fc0
Borhan Uddin <borhaninfos@gmail.com>
2022-03-13 22:00:26 +0600

    update

--- src/components/burgerBuilder/Burger/Burger.js
+++ src/components/burgerBuilder/Burger/Burger.js
@@ -1,13 +1,27 @@
 import React from "react";
+import "./Burger.css";
 import Ingredient from "../Ingrediant/Ingredient";
 
-const Burger = (prosp) => {
+const Burger = (props) => {
+  let ingrediantArr = props.ingredients
+    .map((item) => {
+      let amountArr = [...Array(item.amount).keys()];
+      return amountArr.map((_) => {
+        return <Ingredient type={item.type} key={Math.random()} />;
+      });
+    })
+    .reduce((arr, element) => {
+      return arr.concat(element);
+    }, []);
+
+  if (ingrediantArr.length === 0) {
+    ingrediantArr = <p>Please Add Some Ingredients!</p>;
+  }
+  console.log(ingrediantArr);
   return (
-    <div>
+    <div className="burger">
       <Ingredient type="bread-top" />
-      <Ingredient type="meat" />
-      <Ingredient type="salad" />
-      <Ingredient type="cheese" />
+      {ingrediantArr}
       <Ingredient type="bread-bottom" />
     </div>
   );
