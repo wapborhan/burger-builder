639356455820134be421bbd1d1f4fedbfa8bbdc8
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-12-24 20:11:38 +0600

    Update Auth.js

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 import { Formik } from "formik";
 
 export default class Auth extends Component {
+  state = {
+    mode: "Sign Up",
+  };
+  switchModeHandler = () => {
+    this.setState({
+      mode: this.state.mode === "Sign Up" ? "Login" : "Sign Up",
+    });
+  };
   render() {
     return (
       <div>
@@ -32,11 +40,31 @@ export default class Auth extends Component {
             } else if (values.password.length < 6) {
               errors.password = "Must be atleast 6 Characters";
             }
+            if (this.state.mode === "Sign Up") {
+              // Confirm Password
+              if (!values.passwordconfirm) {
+                errors.passwordconfirm = "Requred";
+              } else if (values.password !== values.passwordconfirm) {
+                errors.passwordconfirm = "Password does not match!";
+              }
+            }
+
+            //
+            // console.log(errors);
+            return errors;
           }}
         >
-          {({ values, handleChange, handleSubmit }) => (
-            <div className="mt-4">
+          {({ values, handleChange, handleSubmit, errors }) => (
+            <div className="mt-4 border rounded p-4">
+              <button
+                className="btn mb-3 text-white w-100"
+                style={{ backgroundColor: "#D70F64" }}
+                onClick={this.switchModeHandler}
+              >
+                Switch to {this.state.mode === "Sign Up" ? "Login" : "Sign Up"}{" "}
+              </button>
               <form onSubmit={handleSubmit}>
+                <span className="text-danger">{errors.email}</span>
                 <input
                   name="email"
                   className="form-control"
@@ -45,6 +73,8 @@ export default class Auth extends Component {
                   onChange={handleChange}
                 />
                 <br />
+                <span className="text-danger">{errors.password}</span>
+
                 <input
                   name="password"
                   className="form-control"
@@ -52,17 +82,26 @@ export default class Auth extends Component {
                   value={values.password}
                   onChange={handleChange}
                 />
-                <br />
-                <input
-                  name="passwordconfirm"
-                  className="form-control"
-                  placeholder="Confirm Password"
-                  value={values.passwordconfirm}
-                  onChange={handleChange}
-                />
+                {this.state.mode === "Sign Up" ? (
+                  <div>
+                    {" "}
+                    <br />
+                    <span className="text-danger">
+                      {errors.passwordconfirm}
+                    </span>
+                    <input
+                      name="passwordconfirm"
+                      className="form-control"
+                      placeholder="Confirm Password"
+                      value={values.passwordconfirm}
+                      onChange={handleChange}
+                    />
+                  </div>
+                ) : null}
+
                 <br />
                 <button type="submit" className="btn btn-success">
-                  Login
+                  {this.state.mode === "Sign Up" ? "Sign Up" : "Login"}
                 </button>
               </form>
             </div>
