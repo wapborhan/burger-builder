ed209e316f2f203aeb8086b639d972b1deebba90
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2023-01-19 10:44:46 +0600

    Update Spinner.js

--- src/components/spinner/Spinner.js
+++ src/components/spinner/Spinner.js
@@ -2,7 +2,14 @@ import React from "react";
 import "./spinner.css";
 
 function Spinner() {
-  return <div className="loader">Loading...</div>;
+  return (
+    <div className="container">
+      <div className="w-100">
+        {" "}
+        <div className="loader">Loading...</div>
+      </div>
+    </div>
+  );
 }
 
 export default Spinner;
